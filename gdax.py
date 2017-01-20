5a91069d22ca17564749bc79686477e06da5c6a4
Radovan Lekanovic <rlekanovic@magicleap.com>
2017-01-19 22:24:24 -0500

    Adding gdax exchange

---
+++ gdax.py
@@ -0,0 +1,44 @@
+# Requires python-requests. Install with pip:
+#
+#   pip install requests
+#
+# or, with easy-install:
+#
+#   easy_install requests
+
+import json, hmac, hashlib, time, requests, base64, os
+from requests.auth import AuthBase
+
+# Create custom authentication for Exchange
+class CoinbaseExchangeAuth(AuthBase):
+    def __init__(self, api_key, secret_key, passphrase):
+        self.api_key = api_key
+        self.secret_key = secret_key
+        self.passphrase = passphrase
+
+    def __call__(self, request):
+        timestamp = str(time.time())
+        message = timestamp + request.method + request.path_url + (request.body or '')
+        hmac_key = base64.b64decode(self.secret_key)
+        signature = hmac.new(hmac_key, message, hashlib.sha256)
+        signature_b64 = signature.digest().encode('base64').rstrip('\n')
+
+        request.headers.update({
+            'CB-ACCESS-SIGN': signature_b64,
+            'CB-ACCESS-TIMESTAMP': timestamp,
+            'CB-ACCESS-KEY': self.api_key,
+            'CB-ACCESS-PASSPHRASE': self.passphrase,
+            'Content-Type': 'application/json'
+        })
+        return request
+
+api_url = 'https://api.gdax.com/'
+
+
+key = os.environ['GDAX_KEY']
+secret = os.environ['GDAX_SECRET']
+passphrase = os.environ['GDAX_PASSPHRASE']
+
+auth = CoinbaseExchangeAuth(key, secret, passphrase)
+r = requests.get(api_url + 'accounts', auth=auth)
+print r.json()
